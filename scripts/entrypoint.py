3978a32c354366996c734cd5b3cd9134551ef005
Isman Firmansyah <isman.firmansyah@gmail.com>
2018-02-07 00:57:08 +0700

    use SSL to connect to LDAP

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -1,6 +1,8 @@
+import base64
 import os
 
 import consulate
+from M2Crypto.EVP import Cipher
 
 GLUU_LDAP_URL = os.environ.get("GLUU_LDAP_URL", "localhost:1636")
 GLUU_KV_HOST = os.environ.get("GLUU_KV_HOST", "localhost")
@@ -20,27 +22,44 @@ def render_salt():
 
 
 def render_ldap_properties():
-    ldap_binddn = consul.kv.get("ldap_binddn")
-    encoded_ox_ldap_pw = consul.kv.get("encoded_ox_ldap_pw")
-    inumAppliance = consul.kv.get("inumAppliance")
-    use_ssl = consul.kv.get("ldap_use_ssl", "false")
-    encoded_openldapJksPass = consul.kv.get("encoded_openldapJksPass")
-
     with open("/opt/templates/ox-ldap.properties.tmpl") as fr:
         txt = fr.read()
 
         with open("/etc/gluu/conf/ox-ldap.properties", "w") as fw:
             rendered_txt = txt % {
-                "ldap_binddn": ldap_binddn,
-                "encoded_ox_ldap_pw": encoded_ox_ldap_pw,
-                "inumAppliance": inumAppliance,
+                "ldap_binddn": consul.kv.get("ldap_binddn"),
+                "encoded_ox_ldap_pw": consul.kv.get("encoded_ox_ldap_pw"),
+                "inumAppliance": consul.kv.get("inumAppliance"),
                 "ldap_url": GLUU_LDAP_URL,
-                "use_ssl": use_ssl,
-                "encoded_openldapJksPass": encoded_openldapJksPass,
+                "ldapTrustStoreFn": consul.kv.get("ldapTrustStoreFn"),
+                "encoded_ldapTrustStorePass": consul.kv.get("encoded_ldapTrustStorePass")
             }
             fw.write(rendered_txt)
+
+
+def decrypt_text(encrypted_text, key):
+    # Porting from pyDes-based encryption (see http://git.io/htpk)
+    # to use M2Crypto instead (see https://gist.github.com/mrluanma/917014)
+    cipher = Cipher(alg="des_ede3_ecb",
+                    key=b"{}".format(key),
+                    op=0,
+                    iv="\0" * 16)
+    decrypted_text = cipher.update(base64.b64decode(
+        b"{}".format(encrypted_text)
+    ))
+    decrypted_text += cipher.final()
+    return decrypted_text
+
+
+def sync_ldap_pkcs12():
+    pkcs = decrypt_text(consul.kv.get("ldap_pkcs12_base64"),
+                        consul.kv.get("encoded_salt"))
+
+    with open(consul.kv.get("ldapTrustStoreFn"), "wb") as fw:
+        fw.write(pkcs)
 
 
 if __name__ == "__main__":
     render_salt()
     render_ldap_properties()
+    sync_ldap_pkcs12()
